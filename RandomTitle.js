6d9627c7364c33de8cc88aa74cae6cf50e59173c
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-20 22:10:15 +0100

    ok

---
+++ RandomTitle.js
@@ -0,0 +1,19 @@
+const titleOptions = [
+    "Focat the noob",
+    "Hydra Network on top",
+    "ss.Spooky.ss is way gooder",
+    "hmmm",
+    "focat is actual",
+    "casual is OkYesOkRageScreamGooderPro",
+    "Buy premium",
+    "if you reading this you like men",
+    "Server noob is noob",
+    "Ur mom",
+    "Rofl",
+    "Tönka",
+    "Lüh pussy boi",
+    "__index is noob",
+    "Idk",
+];
+const randomIndex = Math.floor(Math.random() * titleOptions.length);
+document.title = titleOptions[randomIndex];
